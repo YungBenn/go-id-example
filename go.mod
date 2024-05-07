79224e4792910ee3574352ab2388fb25f375c3f7
YungBenn <adisuryo22@gmail.com>
2024-05-07 22:04:31 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module go-id-test
+
+go 1.22.0
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/lucsky/cuid v1.2.1 // indirect
+	github.com/matoous/go-nanoid/v2 v2.0.0 // indirect
+	github.com/oklog/ulid/v2 v2.1.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/lucsky/cuid"
+	gonanoid "github.com/matoous/go-nanoid/v2"
+	"github.com/oklog/ulid/v2"
+)
+
+func main() {
+	nanoID, err := generateNanoID()
+	if err != nil {
+		panic(err)
+	}
+
+	cuid, err := generateCUID()
+	if err != nil {
+		panic(err)
+	}
+
+	ulid := generateULID()
+
+	uuidv4 := generateUUIDv4()
+
+	uuidv7, err := generateUUIDv7()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("NanoID:", nanoID)
+	fmt.Println("CUID:", cuid)
+	fmt.Println("ULID:", ulid)
+	fmt.Println("UUIDv4:", uuidv4)
+	fmt.Println("UUIDv7:", uuidv7)
+}
+
+func generateUUIDv4() string {
+	uuid := uuid.New()
+	return uuid.String()
+}
+
+func generateUUIDv7() (string, error) {
+	uuidv7, err := uuid.NewV7()
+	if err != nil {
+		return "", err
+	}
+
+	return uuidv7.String(), nil
+}
+
+func generateNanoID() (string, error) {
+	nanoID, err := gonanoid.Generate("abcdefghijklmnopqrstuvwxyz1234567890", 23)
+	if err != nil {
+		return "", nil
+	}
+
+	return nanoID, nil
+}
+
+func generateCUID() (string, error) {
+	cuid, err := cuid.NewCrypto(rand.Reader)
+	if err != nil {
+		return "", err
+	}
+
+	return cuid, nil
+}
+
+func generateULID() string {
+	ulid := ulid.Make()
+	return ulid.String()
+}
